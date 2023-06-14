e4e1e68f04c2c1409798593d9cf35636d8db393f
swagfinger <swagfinger@gmail.com>
2023-06-14 10:49:34 +0800

    basic mongo connection

--- 10-backend-mongodb-no-mongoose/app.js
+++ 10-backend-mongodb-no-mongoose/app.js
@@ -1,31 +1,42 @@
 const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser');
+require('dotenv').config();
 
-const errorController = require('./controllers/error');
+const bodyParser = require('body-parser');
+// const errorController = require('./controllers/error');
+
+const mongoConnect = require('./util/database');
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then((user) => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+// User.findById(1)
+//   .then((user) => {
+//     req.user = user;
+//     next();
+//   })
+//   .catch((err) => console.log(err));
+// });
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
-app.use(errorController.get404);
+// app.use(errorController.get404);
+
+const startConnection = async () => {
+  const connection = await mongoConnect();
+  app.listen(3000);
+  // console.log('connection: ', connection);
+  console.log('listening on port 3000');
+};
+startConnection();
