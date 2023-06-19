53ecfa4f0a77e794e699ea5e1a5a171c305bf04e
clarklindev <clarklindev@gmail.com>
2023-06-19 16:30:55 +0800

    FRONTEND - page layout

--- 13-backend-mongodb-with-mongoose/frontend/src/layouts/HomeLayout.jsx
+++ 13-backend-mongodb-with-mongoose/frontend/src/layouts/HomeLayout.jsx
@@ -2,44 +2,30 @@ import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import { Breadcrumbs } from '../components/Breadcrumbs';
 import { Navbar } from '../components/Navbar';
-import styles from '../components/Navbar.module.css';
 
 export const HomeLayout = () => {
   return (
     <>
       <Navbar>
-        <header>
-          <nav>
-            <NavLink
-              className={styles['nav-link']}
-              activeClassName={styles.active}
-              to='/'
-            >
-              Home
-            </NavLink>
-            <NavLink
-              className={styles['nav-link']}
-              activeClassName={styles.active}
-              to='about'
-            >
-              About
-            </NavLink>
-            <NavLink
-              className={styles['nav-link']}
-              activeClassName={styles.active}
-              to='help'
-            >
-              Help
-            </NavLink>
-            <NavLink
-              className={styles['nav-link']}
-              activeClassName={styles.active}
-              to='todos'
-            >
-              Todos
-            </NavLink>
-          </nav>
-        </header>
+        <div>
+          <NavLink className='nav-link' activeClassName='active' to='/'>
+            Home
+          </NavLink>
+          <NavLink className='nav-link' activeClassName='active' to='about'>
+            About
+          </NavLink>
+          <NavLink className='nav-link' activeClassName='active' to='help'>
+            Help
+          </NavLink>
+          <NavLink className='nav-link' activeClassName='active' to='todos'>
+            Todos
+          </NavLink>
+        </div>
+        <div>
+          <NavLink className='nav-link' activeClassName='active' to='login'>
+            Login
+          </NavLink>
+        </div>
       </Navbar>
       <Breadcrumbs />
 
